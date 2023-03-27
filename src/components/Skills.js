0b34462b536736f6bfec913a5b4badc537f3e148
fatemeRasolzade <fatemerasolzade.1424@gmail.com>
2023-03-27 17:46:26 +0430

    add skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,7 +2,7 @@ const Skills = () => {
   return (
     <>
       <h2 className="title">Skills</h2>
-      <p className="text">HTML, CSS, React js, Next js, Redux, Restful API, GraqhQl, WebSocket</p>
+      <p className="text">HTML, CSS, javaScript, typeScript,  React js, Next js, Redux, Restful API, GraqhQl, WebSocket</p>
       <p className="text pb-10">Material Ui, Tailwind, BootStrap, Git</p>
     </>
   );
