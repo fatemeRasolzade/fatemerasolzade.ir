738d65fbaf53fc9ad12c1647907e127a53957b45
fatemeRasolzade <fatemerasolzade.1424@gmail.com>
2024-01-07 17:49:31 +0330

    Update first page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import "./styles/App.css";
 import SimpleBar from "simplebar-react";
 import "simplebar/dist/simplebar.min.css";
-import Skills from "./components/Skills";
-import AboutMe from "./components/AboutMe";
 import SocialMenu from "./components/SocialMenu";
 import MobileSocialMenu from "./components/MobileSocialMenu";
+import Fatemeh from "./public/images/fatemeh.jpg";
 
 function App() {
   return (
     <SimpleBar style={{ maxHeight: "100vh" }} autoHide={false}>
-      <main className="px-10 sm:px-28 md:px-40 lg:px-52 pt-10 xl:pt-32 text-white relative">
-        <p className="text-base">hello, I am</p>
-        <h1 className="text-6xl sm:text-7xl mt-5 font-msmadi">Fatemeh Rasolzadeh</h1>
-        <p className="mt-5 sm:mt-10 text-lg sm:text-xl font-verdana text-light-purple">Front-end developer, create user friendly website to help businesses do better online</p>
-        <AboutMe />
-        <Skills />
+      <main className="px-10 sm:px-28 md:px-40 lg:px-52 text-white relative flex justify-center items-center gap-20">
+        <div className="flex flex-col justify-start">
+          <p className="text-lg">Hey, Nice to meet you</p>
+          <h1 className="text-6xl sm:text-7xl font-semibold mt-5 font-msmadi">
+            Fatemeh Rasolzadeh
+          </h1>
+          <p className="mt-5 sm:mt-10 text-lg sm:text-xl font-verdana text-main-blue">
+            Front-end developer, create user friendly website to help businesses
+            do better online
+          </p>
+        </div>
+        <img
+          src={Fatemeh}
+          alt="fatemeh"
+          width={500}
+          className="rounded-b-full"
+        />
+        {/* <AboutMe /> */}
+        {/* <Skills /> */}
         <SocialMenu />
         <MobileSocialMenu />
       </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        'dark-purple': '#24203b',
-        'light-purple': '#9189b3',
-        'main-purple': '#b266d2',
+        "dark-purple": "#24203b",
+        "light-purple": "#9189b3",
+        "main-purple": "#b266d2",
+        "dark-blue": "#100F17",
+        "main-blue": "#66788D",
       },
       fontFamily: {
-        'oswald': ['oswald'],
-        'msmadi': ['msmadi'],
-        'verdana': ['verdana'],
-
-      }
+        oswald: ["oswald"],
+        msmadi: ["msmadi"],
+        verdana: ["verdana"],
+      },
     },
   },
   plugins: [],
-}
+};
