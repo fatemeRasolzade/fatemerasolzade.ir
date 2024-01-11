42ff8e37f0335a763761b47fa1a69084f4c0e983
fatemeRasolzade <fatemerasolzade.1424@gmail.com>
2024-01-11 23:19:35 +0330

    Added text for first page

--- src/App.js
+++ src/App.js
@@ -8,23 +8,21 @@ import Fatemeh from "./public/images/fatemeh.jpg";
 function App() {
   return (
     <SimpleBar style={{ maxHeight: "100vh" }} autoHide={false}>
-      <main className="px-10 sm:px-28 md:px-40 lg:px-52 text-white relative flex justify-center items-center gap-20">
+      <main className="mx-auto max-w-7xl text-white relative flex justify-center items-center gap-32">
         <div className="flex flex-col justify-start">
-          <p className="text-lg">Hey, Nice to meet you</p>
-          <h1 className="text-6xl sm:text-7xl font-semibold mt-5 font-msmadi">
-            Fatemeh Rasolzadeh
-          </h1>
-          <p className="mt-5 sm:mt-10 text-lg sm:text-xl font-verdana text-main-blue">
-            Front-end developer, create user friendly website to help businesses
-            do better online
+          <p className="text-2xl">Hello there!</p>
+          <h1 className="text-6xl sm:text-5xl font-semibold mt-4">I'm Fatemeh Rasolzadeh</h1>
+          <h2 className="text-6xl sm:text-5xl font-semibold mt-2">Front-End Developer</h2>
+          <p className="mt-5 text-lg sm:text-base font-verdana opacity-50 text-justify">
+            I'm a programmer, I make love with codes and live with my projects.
+            <br /> It's not just a job but a lifestyle, in fact I can't imagine life without it.
+            <br /> it's a never-ending world with new and tough challenges every day,
+            <br /> never before facing They are not afraid and exciting and attractive to me.
           </p>
+          <p className="text-xl text-main-blue mt-4 cursor-pointer">{`<Download CV />`}</p>
         </div>
-        <img
-          src={Fatemeh}
-          alt="fatemeh"
-          width={500}
-          className="rounded-b-full"
-        />
+
+        <img src={Fatemeh} alt="fatemeh" width={500} className="rounded-b-full border-t" />
         {/* <AboutMe /> */}
         {/* <Skills /> */}
         <SocialMenu />
