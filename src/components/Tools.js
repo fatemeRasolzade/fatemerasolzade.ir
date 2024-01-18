75e22514f030f36cdfbf819fc0744ccaaf1e1140
fatemeRasolzade <fatemerasolzade.1424@gmail.com>
2024-01-18 20:39:41 +0330

    :sparkles: Added text description for tools

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -5,7 +5,10 @@ const Tools = () => {
     <main className="mx-auto max-w-7xl text-white relative flex justify-center items-center gap-32">
       <div className="flex flex-col justify-start">
         <h3 className="text-6xl sm:text-5xl font-semibold mt-4 text-center">Tools & requirements</h3>
-        <p className="mt-5 text-lg sm:text-base font-verdana opacity-50 text-center">tools lorem ipsum</p>
+        <p className="mt-5 text-lg sm:text-base font-verdana opacity-50 text-center">
+          I need professional and trendy tools for coding, team work, testing and generating backend APIs,
+          recording tasks I do and implementing the designer plan.
+        </p>
         <div className="flex flex-col gap-8 mt-10">
           <div className="flex gap-8 h-20 justify-center">
             {tools.map((item) => (
