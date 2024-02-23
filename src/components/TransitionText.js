6bfa25e815b601c0ce26fad43b3f229f60b8c091
fatemeRasolzade <fatemerasolzade.1424@gmail.com>
2024-02-23 23:13:07 +0330

    :iphone: Responsived about me page

--- src/components/TransitionText.js
+++ src/components/TransitionText.js
@@ -1,7 +1,7 @@
 const TransitionText = ({ text, wrapperClassName }) => {
   const splitedText = text.split("");
   return (
-    <p className={`text-6xl sm:text-5xl font-semibold select-none flex  ${wrapperClassName}`}>
+    <p className={`text-4xl md:text-5xl font-semibold select-none flex  ${wrapperClassName}`}>
       {splitedText.map((item) => (
         <span className={`icon-transition block ${item === " " ? "w-3" : ""}`}>{item}</span>
       ))}
--- src/containers/AboutMe.js
+++ src/containers/AboutMe.js
@@ -8,12 +8,12 @@ const AboutMe = () => {
   return (
     <div className="section">
       <div className="relative">
-        <div className="mx-auto max-w-7xl text-white !flex justify-center items-center animate-banner gap-32">
+        <div className="mx-auto max-w-7xl text-white !flex justify-center items-center animate-banner gap-10 md:gap-32">
           <div className="flex flex-col justify-start min-w-[38rem]">
             <p className="text-2xl">Hello there!</p>
             <TransitionText text="I'm Fatemeh Rasolzadeh" wrapperClassName="mt-4" />
             <TransitionText text="Front-End Developer" wrapperClassName="mt-2" />
-            <p className="mt-5 text-lg sm:text-base font-verdana opacity-50 text-justify">
+            <p className="mt-5 text-sm md:text-base font-verdana opacity-50 text-justify">
               I'm a programmer, I make love with codes and live with my projects.
               <br /> It's not just a job but a lifestyle, in fact I can't imagine life without it.
               <br /> it's a never-ending world with new and tough challenges every day,
@@ -21,11 +21,11 @@ const AboutMe = () => {
             </p>
             <a
               download={"fatemeRasolzade.pdf"}
-              className="text-xl text-main-blue mt-4 cursor-pointer icon-transition"
+              className="text-lg md:text-xl text-main-blue mt-4 cursor-pointer icon-transition"
               href={"../assets/documents/resume.pdf"}
             >{`<Download CV />`}</a>
           </div>
-          <img src={Fatemeh} alt="fatemeh" width={500} className="rounded-b-full border-t" />
+          <img src={Fatemeh} alt="fatemeh" width={500} className="rounded-b-full border-t hidden md:inline" />
         </div>
         <Fragment className={"absolute -top-56 left-[107rem] blur-[6px] text-main-orange"} />
         <Code className={"absolute top-[23.4rem] left-[67rem] blur-[6px]"} />
